3759f67af466986ca263ccb464f075d2f7c13840
Payal120324 <146610738+Payal120324@users.noreply.github.com>
2026-04-26 20:17:28 +0530

    Update loader.py

--- src/data/loader.py
+++ src/data/loader.py
@@ -3,10 +3,10 @@ import pandas as pd
 def load_data(filepath):
     df = pd.read_csv(filepath)
     
-    # Convert Date column
+    
     df['Date'] = pd.to_datetime(df['Date'])
     
-    # Sort properly
+   
     df = df.sort_values('Date')
     
     return df
